f70fb2b3d9a4af92cbcb24a3252177c713a49feb
Arfak150 <arfakgamer@hotmail.com>
2023-02-14 08:48:21 -0300

    correccion error en url

--- src/main/java/com/portfolio/facu/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/facu/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://private-arda-arfak150.koyeb.app/")
+@CrossOrigin(origins = "https://private-arda-arfak150.koyeb.app")
 /* OTRO LINK ----- http://localhost:4200 ----- */
 public class CExperiencia {
     @Autowired
