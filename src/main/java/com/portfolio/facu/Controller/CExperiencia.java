52de204d6797ec2083b14a2947291dd7cee906a2
Arfak150 <arfakgamer@hotmail.com>
2023-02-14 09:55:55 -0300

    cambio de nuevo a render

--- src/main/java/com/portfolio/facu/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/facu/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://portfolioback.private-arda.koyeb")
+@CrossOrigin(origins = "https://backfacu.onrender.com")
 /* OTRO LINK ----- http://localhost:4200 ----- */
 public class CExperiencia {
     @Autowired
--- src/main/java/com/portfolio/facu/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/facu/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://portfolioback.private-arda.koyeb")
+@CrossOrigin(origins = "https://backfacu.onrender.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
