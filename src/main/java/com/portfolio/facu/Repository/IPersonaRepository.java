5fa55104d4a57274b508ebf220ec9cf6c6e5899a
Arfak150 <arfakgamer@hotmail.com>
2023-05-10 16:02:36 -0300

    Retomando

--- src/main/java/com/portfolio/facu/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/facu/Repository/IPersonaRepository.java
@@ -2,9 +2,13 @@
 package com.portfolio.facu.Repository;
 
 import com.portfolio.facu.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+public interface IPersonaRepository extends JpaRepository<Persona, Integer>{
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+    
 }
